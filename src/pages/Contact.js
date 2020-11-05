3a6cd93d23ebe30f40027e74705efb71d3217e2f
Evelyn Goris <goris.evelyn@gmail.com>
2020-11-04 23:23:28 -0600

    created Contact.js file and implemented it

---
+++ src/pages/Contact.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+
+export default class Contact extends Component {
+  render() {
+    return (
+      <section id="about" className="section scrollspy">
+        <h3 className="page-title white-text teal">Contact Me</h3>
+        <div className="container flow-text">
+          <blockquote>
+            <h2>
+              Email:{" "}
+              <a
+                href="mailto:goris.evelyn@gmail.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                goris.evelyn@gmail.com
+              </a>{" "}
+              <i className="fas fa-external-link-alt" />
+            </h2>
+            <h2>
+              Github:{" "}
+              <a
+                href="https://www.github.com/SIROG-E"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                github.com/SIROG-E
+              </a>
+            </h2>
+            <h2>
+              LinkedIn:{" "}
+              <a
+                href="https://www.linkedin.com/in/evelyn-goris-78668a1b5/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                linkedin.com/in/evelyn-goris-78668a1b5/
+              </a>
+            </h2>
+          </blockquote>
+        </div>
+      </section>
+    );
+  }
+}
