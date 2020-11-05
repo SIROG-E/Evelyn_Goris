49a1d68c09f45789b8856f22aa58e8decd4eb134
Evelyn Goris <goris.evelyn@gmail.com>
2020-11-04 23:00:27 -0600

    created Landind.js and implemented it

---
+++ src/pages/Landing.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import mobile from 'is-mobile'
+
+export default class Landing extends Component {
+  mainImage = mobile() ? <img alt="Evelyn Goris" className="mainImage" src="static/img/mainImage/Me.jpg" />
+    : <img alt="Evelyn Goris" className="mainImage" src="static/img/mainImage/Me.jpg" />
+  render() {
+    return (
+      <section id="intro" className="section scrollspy full-height">
+        <div className="overlay">
+          {this.mainImage}
+        </div>
+        <div className="container">
+          <h2>Hello World, I'm Evelyn! </h2>
+        </div>
+      </section>
+    )
+  }
+}
