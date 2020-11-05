7571943328d759e0770ef0066b2761a3436dd713
Evelyn Goris <goris.evelyn@gmail.com>
2020-11-04 23:35:42 -0600

    created skillData file and implemented it

---
+++ src/Content/SkillData/index.js
@@ -0,0 +1,21 @@
+export default {
+  programmingLanguages: [
+    { name: "Javascript", image: "javascript.png" },
+  ],
+  webDevelopment: [
+    { name: "Node", image: "nodejs.png" },
+    { name: "React", image: "react.png" },
+    { name: "JQuery", image: "jquery.png" },
+    { name: "HTML5", image: "html5.png" },
+    { name: "MySQL", image: "mysql.png" },
+    { name: "CSS3", image: "css3.png" },
+    { name: "Bootstrap", image: "bootstrap.png" }
+  ],
+  databases: [
+    { name: "MongoDB", image: "mongodb.png" },
+    { name: "MySQL", image: "mysql.png" },
+  ],
+  otherSkills: [
+    { name: "GitHub", image: "github.png" },
+  ]
+};
