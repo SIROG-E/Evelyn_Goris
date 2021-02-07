bedd3409c80d6ae0d1de652d6befb59697cf6175
Evelyn Goris <goris.evelyn@gmail.com>
2021-02-07 03:27:51 -0600

    update about info... Just got my certificate woo hoo

--- src/Content/AboutData/AboutCard.js
+++ src/Content/AboutData/AboutCard.js
@@ -3,6 +3,6 @@ export default {
   about: "Hi! I am Evelyn Goris",
   developments: [],
   accomplishments: [
-    "I am originally from Peru, South America. I am currently enrolled in Coding Bootcamp at Georgia Institute of Technology earning a certificate in Coding. I hope that this portfolio provides insight into my character, programming background, analytical skills, and accomplishments. After graduating from the Bootcamp program, I would like to obtain a position as a Software Developer to utilize what I learned in programming. In this portfolio, I am aiming to:",
+    "I am originally from Peru, South America. I recently graduated from Georgia Institute of Technology. I am a Software Engineer based in Houston, TX specializing in building applications, websites, and designing. I hope that this portfolio provides insight into my character, programming background, analytical skills, and accomplishments. I would like to obtain a position as a Software Developer to utilize what I learned in programming. In this portfolio, I am aiming to:",
   ],
 };
