61a9862ba176d9ee678dd72e6a04af5b1803a774
Evelyn Goris <goris.evelyn@gmail.com>
2021-02-07 03:39:19 -0600

    updated

--- src/Content/ProjectData/employeeDirectory.js
+++ src/Content/ProjectData/employeeDirectory.js
@@ -5,7 +5,7 @@ export default {
   image: "Employee_Directory_Walkthrough.gif",
   accomplishments: [
     "This Employee Directory App was created with React. It allows the user to sort the table by name, phone number, email and date of birth. It also allows user to search for a specific employee.",
-    "Random User Api was used for generating random user data. ."
+    "Random User Api was used for generating random user data."
   ],
   liveLink: "https://sirog-e.github.io/employee-directory/",
   githubLink: "https://github.com/SIROG-E/employee-directory"
